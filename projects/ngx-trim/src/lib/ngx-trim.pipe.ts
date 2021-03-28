f6f1712ebe5948a1ffb33c391a49726b333a801d
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2021-03-28 19:54:52 +0900

    :sparkles: Export trim function
    to use trimming logic directly in service.

--- projects/ngx-trim/src/lib/ngx-trim.pipe.ts
+++ projects/ngx-trim/src/lib/ngx-trim.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+export function trim(value: string): string {
+  return value.trim();
+}
+
 @Pipe({
   name: 'ngxTrim'
 })
 export class NgxTrimPipe implements PipeTransform {
 
   transform(value: string): string {
-    return value.trim();
+    return trim(value);
   }
 
 }
